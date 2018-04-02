ea797080aedf95c7692a50aaada3594ca237c0fe
YanC Lin <yonalin1@gmail.com>
2018-04-02 04:21:05 +0000

    automatically backed up by learn

--- db/migrate/20180402042016_create_titles.rb
+++ db/migrate/20180402042016_create_titles.rb
@@ -1,4 +1,7 @@
-class CreateTitles < ActiveRecord::Migration[5.1]
+class CreateTitles < ActiveRecord::Migration
   def change
+    create_table :titles do |t|
+      t.string :name
+    end
   end
 end
